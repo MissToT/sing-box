950d01d88ba21f822790a3bae93c50c19cb1a490
MissToT <115005968+MissToT@users.noreply.github.com>
2026-09-18 16:24:33 +0800

    添加 TCP并发

--- adapter/network.go
+++ adapter/network.go
@@ -47,6 +47,7 @@ type NetworkOptions struct {
 	NetworkType          []C.InterfaceType
 	FallbackNetworkType  []C.InterfaceType
 	FallbackDelay        time.Duration
+	TCPConcurrentDial    bool
 }
 
 type InterfaceUpdateListener interface {
--- protocol/direct/outbound.go
+++ protocol/direct/outbound.go
@@ -33,6 +33,7 @@ func RegisterOutbound(registry *outbound.Registry) {
 var (
 	_ N.ParallelDialer                   = (*Outbound)(nil)
 	_ dialer.ParallelNetworkDialer       = (*Outbound)(nil)
+	_ dialer.ConcurrentNetworkDialer     = (*Outbound)(nil)
 	_ dialer.DirectDialer                = (*Outbound)(nil)
 	_ adapter.FlowOutbound               = (*Outbound)(nil)
 	_ adapter.FlowOutboundDomainResolver = (*Outbound)(nil)
@@ -352,6 +353,23 @@ func (h *Outbound) DialParallelNetwork(ctx context.Context, network string, dest
 		}
 	}
 	return conn, nil
+}
+
+func (h *Outbound) DialConcurrentNetwork(ctx context.Context, network string, destination M.Socksaddr, destinationAddresses []netip.Addr, networkStrategy *C.NetworkStrategy, networkType []C.InterfaceType, fallbackNetworkType []C.InterfaceType, fallbackDelay time.Duration) (net.Conn, error) {
+	if h.isMyLoopbackAddress(destinationAddresses...) {
+		return nil, E.New("loopback connection to TUN range")
+	}
+	ctx, metadata := adapter.ExtendContext(ctx)
+	metadata.Outbound = h.Tag()
+	metadata.Destination = destination
+	network = N.NetworkName(network)
+	switch network {
+	case N.NetworkTCP:
+		h.logger.InfoContext(ctx, "outbound connection to ", destination)
+	case N.NetworkUDP:
+		h.logger.InfoContext(ctx, "outbound packet connection to ", destination)
+	}
+	return dialer.DialConcurrentNetwork(ctx, h.dialer, h.logger, network, destination, destinationAddresses, networkStrategy, networkType, fallbackNetworkType, fallbackDelay)
 }
 
 func (h *Outbound) ListenSerialNetworkPacket(ctx context.Context, destination M.Socksaddr, destinationAddresses []netip.Addr, networkStrategy *C.NetworkStrategy, networkType []C.InterfaceType, fallbackNetworkType []C.InterfaceType, fallbackDelay time.Duration) (net.PacketConn, netip.Addr, error) {
